c69896ff77b0e9f68bea730710ea8314fdd78225
Drew Cook <drewcook29@gmail.com>
2021-05-10 15:52:12 -0600

    reviews with randomuser.me

---
+++ components/Reviews.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="reviews">
+    <h3>Customer Reviews</h3>
+    <p v-if="$fetchState.pending">Loading...</p>
+    <p v-else-if="$fetchState.error">An error occurred :(</p>
+    <div v-else>
+      <div v-if="reviews.results">
+        <Review v-for="review in reviews.results" :key="review.id.value" :review="review" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      reviews: null
+    }
+  },
+  async fetch() {
+    this.reviews = await fetch('https://randomuser.me/api?results=5').then(res => res.json())
+  }
+}
+</script>
+
+<style>
+
+</style>
