f5253de2b82b8b95eca7168cf2aabecfeb8f4029
Drew Cook <drewcook29@gmail.com>
2021-05-10 10:50:05 -0600

    add in mock guitar data

---
+++ assets/data.js
@@ -0,0 +1,169 @@
+export const largeCardSections = [
+	{
+		id: 1,
+		title: 'Top-rated Guitars in Denver',
+		snippet: 'User say these are the best guitars to rent if you have a last minute gig.',
+		cards: [
+			{
+				id: 19,
+				title: 'Fender Professional Stratocaster',
+				snippet:
+					"If there was a list of classic guitars to own, this would be on there. It's the gold standard for single-coil tonage.",
+				image: 'eg1.jpg',
+			},
+			{
+				id: 20,
+				title: 'Fender Player Jaguar',
+				snippet:
+					'The player jaguar offers a single-coil pickup at the neck position and a humbucking pickup a the bridge for multiple tonal options.',
+				image: 'eg2.jpg',
+			},
+			{
+				id: 21,
+				title: 'Fender Custom HH Telecaster',
+				snippet:
+					'This middle-tier telecaster contains all the classic features, but sports two humbucking pickups to offer a thicker, warmer tone.',
+				image: 'eg3.jpg',
+			},
+		],
+	},
+	{
+		id: 2,
+		title: 'Luxury Electric Guitars',
+		snippet: 'Show up to the gig in style with these high-end, luxary style guitars.',
+		cards: [
+			{
+				id: 22,
+				title: "Vintage 1950's Fender Jazzmaster",
+				snippet:
+					"This guitar contains three P90 style pickups with the 1950's C-shaped neck profile in a mint green finish.",
+				image: 'eg4.jpg',
+			},
+			{
+				id: 23,
+				title: 'Metallic Onyx Fender Telecaster',
+				snippet:
+					'This one is made out of pure onyx. The metal choice was a poor for functionality, but was great for style!',
+				image: 'eg5.jpg',
+			},
+			{
+				id: 24,
+				title: 'Champagne Fender Mustang',
+				snippet:
+					'This is only Fender Mustang ever made with champagne finish, perfect for bringing in the New Year.',
+				image: 'eg6.jpg',
+			},
+		],
+	},
+]
+
+export const smallCardSections = [
+	{
+		id: 1,
+		title: 'Electric Guitars in Chicago',
+		cards: [
+			{
+				id: 1,
+				image: 'eg7.jpg',
+			},
+			{
+				id: 2,
+				image: 'eg8.jpg',
+			},
+			{
+				id: 3,
+				image: 'eg9.jpg',
+			},
+			{
+				id: 4,
+				image: 'eg10.jpg',
+			},
+			{
+				id: 5,
+				image: 'eg11.jpg',
+			},
+			{
+				id: 6,
+				image: 'eg12.jpg',
+			},
+			{
+				id: 7,
+				image: 'eg13.jpg',
+			},
+			{
+				id: 8,
+				image: 'eg14.jpg',
+			},
+			{
+				id: 9,
+				image: 'eg15.jpg',
+			},
+			{
+				id: 10,
+				image: 'eg16.jpg',
+			},
+			{
+				id: 11,
+				image: 'eg17.jpg',
+			},
+			{
+				id: 12,
+				image: 'eg18.jpg',
+			},
+		],
+	},
+	{
+		id: 2,
+		title: 'Electric Guitars in New York',
+		cards: [
+			{
+				id: 13,
+				image: 'eg19.jpg',
+			},
+			{
+				id: 14,
+				image: 'eg20.jpg',
+			},
+			{
+				id: 15,
+				image: 'eg21.jpg',
+			},
+			{
+				id: 16,
+				image: 'eg22.jpg',
+			},
+			{
+				id: 17,
+				image: 'eg23.jpg',
+			},
+			{
+				id: 18,
+				image: 'eg24.jpg',
+			},
+			{
+				id: 19,
+				image: 'eg1.jpg',
+			},
+			{
+				id: 20,
+				image: 'eg2.jpg',
+			},
+			{
+				id: 21,
+				image: 'eg3.jpg',
+			},
+			{
+				id: 22,
+				image: 'eg4.jpg',
+			},
+			{
+				id: 23,
+				image: 'eg5.jpg',
+			},
+			{
+				id: 24,
+				image: 'eg6.jpg',
+			},
+		],
+	},
+]
