a45fba48dba694820be8e0bc1387012eea89b5d6
Drew Cook <drewcook29@gmail.com>
2021-05-10 11:03:09 -0600

    add in navbar

---
+++ components/Nav.vue
@@ -0,0 +1,32 @@
+<template>
+	<header>
+		<b-navbar toggleable="lg" type="dark" variant="dark">
+			<div class="container">
+				<b-navbar-brand href="/">Guitar Exchange</b-navbar-brand>
+
+				<b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+				<b-collapse id="nav-collapse" is-nav>
+					<b-navbar-nav>
+						<b-nav-item href="/products">Products</b-nav-item>
+						<b-nav-item href="/my-items">My Items</b-nav-item>
+					</b-navbar-nav>
+				</b-collapse>
+			</div>
+		</b-navbar>
+	</header>
+</template>
+
+<script>
+	export default {}
+</script>
+
+<style scoped>
+	header {
+		margin-bottom: 50px;
+	}
+
+  #nav-collapse {
+    justify-content: flex-end;
+  }
+</style>
