9bb9ada7b6a495b694e2ab36741d342756e18d11
Drew Cook <drewcook29@gmail.com>
2021-05-10 17:03:17 -0600

    unique keys

--- pages/index.vue
+++ pages/index.vue
@@ -3,12 +3,12 @@
 		<Hero />
 		<LargeCardDisplay
 			v-for="cardInfo in largeCardInfo"
-			:key="cardInfo.id"
+			:key="`large-${cardInfo.id}`"
 			:cardsSection="cardInfo"
 		/>
 		<SmallCardDisplay
 			v-for="cardInfo in smallCardInfo"
-			:key="cardInfo.id"
+			:key="`small-${cardInfo.id}`"
 			:cardsSection="cardInfo"
 		/>
 	</div>
